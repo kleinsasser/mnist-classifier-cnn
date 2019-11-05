52ad511139ac1598ec4d352482788a84fa4b5e71
maxkleinsasser <maxwell.kleinsasser@gmail.com>
2019-11-04 23:23:52 -0600

    training implemented, model doesn't learn :(

--- model.py
+++ model.py
@@ -4,16 +4,23 @@ import idx2numpy as idx
 import pandas as pd
 import torch
 import torch.nn.functional as F
+import torchvision as vision
 
 # load training data
 train_x_np = idx.convert_from_file('t10k-images-idx3-ubyte')
 train_y_np = idx.convert_from_file('t10k-labels-idx1-ubyte')
 
-# one-hot encoding on training data
+# add empty dimensions to training data to fit batch and channel dimension req
+train_x_np = train_x_np[:, None, None, :, :]
+
+# one-hot encoding on training labels
 train_y_pd = pd.DataFrame(train_y_np)
 train_y_pd.columns = ['class']
 train_y_pd = pd.get_dummies(train_y_pd['class'])
 train_y_np = np.array(train_y_pd)
+
+# add empty dimension
+train_y_np = train_y_np[:, None, :]
 
 train_x = torch.Tensor(train_x_np)
 train_y = torch.Tensor(train_y_np)
@@ -26,19 +33,19 @@ class CNN(torch.nn.Module):
         
         # model layers
         # 1 input channel (b/w input), 6 output channels
-        self.conv1 = torch.nn.Conv2d(1, 6, 5, 1)
+        self.conv1 = torch.nn.Conv2d(1, 20, 5, 1)
 
         # nn hidden layer 1
-        self.linear1 = torch.nn.Linear(6 * 14 * 14, 64)
+        self.linear1 = torch.nn.Linear(11520, 128)
         # nn hidden layer to out, 10 classes
-        self.linear2 = torch.nn.Linear(64, 10)
+        self.linear2 = torch.nn.Linear(128, 10)
 
     def forward(self, x):
         # calculate convolution w/ relu activation
         x = F.relu(self.conv1(x))
 
-        # reshape pooled data to fit linear layer
-        x = x.view(-1, 6 * 14 * 14)
+        # reshape data to fit linear layer
+        x = x.view(-1, 11520)
 
         # feed first layer
         x = F.relu(self.linear1(x))
@@ -50,49 +57,56 @@ class CNN(torch.nn.Module):
 model = CNN()
 
 # loss function and optimizer
-loss = torch.nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+loss = torch.nn.MSELoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=0.01)
 
 # choo choo
 order = np.arange(0, 10000, dtype=int)
 
-print(order)
-
 total = 0
 correct = 0
+batch_correct = 0
 
-for i in order:
-    x = train_x[i]
-    y = train_y[i]
+x1 = []
+x2 = []
 
-    # set optimizer gradients to 0
-    optimizer.zero_grad()
+for j in range(1):
+    for i in order:
+        x = train_x[i]
+        y = train_y[i]
 
-    # make prediction
-    y_pred = model(x)
+        print(train_x[i].numpy())
+        print(train_y[i].numpy())
 
-    # calculate loss, backpropagate
-    l = loss(y_pred, y)
-    l.backward()
-    optimizer.step()
+        # make prediction
+        y_pred = model(x)
 
-    total += 1
+        # calculate loss, backpropagate
+        l = loss(y_pred, y)
+        l.backward()
+        optimizer.step()
 
-    # check if correct
-    pred_arr = y_pred.detach().numpy()
-    ans_arr = y.numpy()
+        total += 1
 
-    pred = np.argmax(pred_arr)
-    ans = np.argmax(ans_arr)
+        # check if correct
+        pred_arr = y_pred.detach().numpy()
+        ans_arr = y.numpy()
 
-    if ans == pred:
-        correct += 1
+        pred = np.argmax(pred_arr)
+        ans = np.argmax(ans_arr)
 
-    if total % 100 == 0:
-        print('AFTER: ', total)
-        print('Loss: ', l.item())
-        print('Accuracy', correct / total)
+        if ans == pred:
+            correct += 1
+            batch_correct += 1
 
+        if total % 100 == 0:
+            print('EPOCH: ', j)
+            print('ITERATION: ', total)
+            print('Accuracy', batch_correct / 100)
+            batch_correct = 0
+    print('\nEPOCH 1 ACCURACY: ', correct / total)
+    correct = 0
+    total = 0
 
 
 
