4cfb555bd6e203b442fc17ae506a0f841dc7e1cb
maxkleinsasser <maxwell.kleinsasser@gmail.com>
2019-11-05 22:26:25 -0600

    Complete overhaul, works

--- model.py
+++ model.py
@@ -1,3 +1,6 @@
+# the following model is heavily borrowed from this tutorial:
+# https://nextjournal.com/gkoehler/pytorch-mnist
+
 import numpy as np
 import pandas as pd
 import idx2numpy as idx
@@ -5,25 +8,32 @@ import pandas as pd
 import torch
 import torch.nn.functional as F
 import torchvision as vision
+import sys # TODO: get rid of this
 
-# load training data
-train_x_np = idx.convert_from_file('t10k-images-idx3-ubyte')
-train_y_np = idx.convert_from_file('t10k-labels-idx1-ubyte')
+# hyperparameters
+n_epochs = 3
+batch_size_train = 64
+batch_size_test = 1000
+learning_rate = 0.01
+momentum = 0.5
+log_interval = 10
 
-# add empty dimensions to training data to fit batch and channel dimension req
-train_x_np = train_x_np[:, None, None, :, :]
+# load data with PyTorch data loaders
+train_loader = torch.utils.data.DataLoader(
+    vision.datasets.MNIST('', train=True, download=True,
+    transform=vision.transforms.Compose([
+        vision.transforms.ToTensor(),
+        vision.transforms.Normalize((0.1307,),(0.3081,))
+    ])),
+    batch_size=batch_size_train, shuffle=True)
 
-# one-hot encoding on training labels
-train_y_pd = pd.DataFrame(train_y_np)
-train_y_pd.columns = ['class']
-train_y_pd = pd.get_dummies(train_y_pd['class'])
-train_y_np = np.array(train_y_pd)
-
-# add empty dimension
-train_y_np = train_y_np[:, None, :]
-
-train_x = torch.Tensor(train_x_np)
-train_y = torch.Tensor(train_y_np)
+test_loader = torch.utils.data.DataLoader(
+    vision.datasets.MNIST('', train=False, download=True,
+    transform=vision.transforms.Compose([
+        vision.transforms.ToTensor(),
+        vision.transforms.Normalize((0.1307,),(0.3081,))
+    ])),
+    batch_size=batch_size_test, shuffle=True)
 
 # define model
 class CNN(torch.nn.Module):
@@ -32,82 +42,69 @@ class CNN(torch.nn.Module):
         super(CNN, self).__init__()
         
         # model layers
-        # 1 input channel (b/w input), 6 output channels
-        self.conv1 = torch.nn.Conv2d(1, 20, 5, 1)
+        self.conv1 = torch.nn.Conv2d(1, 10, kernel_size=5)
+        self.conv2 = torch.nn.Conv2d(10, 20, kernel_size=5)
+        self.conv2_drop = torch.nn.Dropout2d()
 
         # nn hidden layer 1
-        self.linear1 = torch.nn.Linear(11520, 128)
+        self.fc1 = torch.nn.Linear(320, 50)
         # nn hidden layer to out, 10 classes
-        self.linear2 = torch.nn.Linear(128, 10)
+        self.fc2 = torch.nn.Linear(50, 10)
 
     def forward(self, x):
-        # calculate convolution w/ relu activation
-        x = F.relu(self.conv1(x))
-
-        # reshape data to fit linear layer
-        x = x.view(-1, 11520)
-
-        # feed first layer
-        x = F.relu(self.linear1(x))
-        # output
-        x = self.linear2(x)
-        return (x)
+        x = F.relu(F.max_pool2d(self.conv1(x), 2))
+        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
+        x = x.view(-1, 320)
+        x = F.relu(self.fc1(x))
+        x = F.dropout(x, training=self.training)
+        x = self.fc2(x)
+        return F.log_softmax(x)
 
 # instantiate model
-model = CNN()
+network = CNN()
 
 # loss function and optimizer
-loss = torch.nn.MSELoss()
-optimizer = torch.optim.SGD(model.parameters(), lr=0.01)
+optimizer = torch.optim.SGD(network.parameters(), lr=learning_rate, momentum=momentum)
 
 # choo choo
-order = np.arange(0, 10000, dtype=int)
+train_losses = []
+train_counter = []
+test_losses = []
+test_counter = [i*len(train_loader.dataset) for i in range(n_epochs + 1)]
 
-total = 0
-correct = 0
-batch_correct = 0
-
-x1 = []
-x2 = []
-
-for j in range(1):
-    for i in order:
-        x = train_x[i]
-        y = train_y[i]
-
-        print(train_x[i].numpy())
-        print(train_y[i].numpy())
-
-        # make prediction
-        y_pred = model(x)
-
-        # calculate loss, backpropagate
-        l = loss(y_pred, y)
-        l.backward()
+def train(epoch):
+    network.train()
+    for batch_idx, (data, target) in enumerate(train_loader):
+        optimizer.zero_grad()
+        output = network(data)
+        loss = F.nll_loss(output, target)
+        loss.backward()
         optimizer.step()
+        if batch_idx % log_interval == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                100. * batch_idx / len(train_loader), loss.item()))
+            train_losses.append(loss.item())
+            train_counter.append(
+                (batch_idx*64) + ((epoch-1)*len(train_loader.dataset)))
 
-        total += 1
+def test():
+  network.eval()
+  test_loss = 0
+  correct = 0
+  with torch.no_grad():
+    for data, target in test_loader:
+      output = network(data)
+      test_loss += F.nll_loss(output, target, size_average=False).item()
+      pred = output.data.max(1, keepdim=True)[1]
+      correct += pred.eq(target.data.view_as(pred)).sum()
+  test_loss /= len(test_loader.dataset)
+  test_losses.append(test_loss)
+  print('\nTest set: Avg. loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+    test_loss, correct, len(test_loader.dataset),
+    100. * correct / len(test_loader.dataset)))
 
-        # check if correct
-        pred_arr = y_pred.detach().numpy()
-        ans_arr = y.numpy()
-
-        pred = np.argmax(pred_arr)
-        ans = np.argmax(ans_arr)
-
-        if ans == pred:
-            correct += 1
-            batch_correct += 1
-
-        if total % 100 == 0:
-            print('EPOCH: ', j)
-            print('ITERATION: ', total)
-            print('Accuracy', batch_correct / 100)
-            batch_correct = 0
-    print('\nEPOCH 1 ACCURACY: ', correct / total)
-    correct = 0
-    total = 0
-
-
-
-
+test()
+for epoch in range(1, n_epochs + 1):
+  train(epoch)
+  test()
